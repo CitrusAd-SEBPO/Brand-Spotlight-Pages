c9d31e18b05263c8b0715fca2eb57f69928fddb3
Imran-Ahmed <stimran4405@gmail.com>
2023-04-26 10:08:41 +0600

    education desktop version done

--- Education-FAQ/assets/custom.js
+++ Education-FAQ/assets/custom.js
@@ -103,3 +103,31 @@ function addActiveClass(element, activeClass) {
     });
   }
 }*/
+
+// accordion js
+
+const accordionItems = document.querySelectorAll(
+  ".citrusSpotlight__accordion-item"
+);
+
+accordionItems.forEach((item) => {
+  const title = item.querySelector(".citrusSpotlight__accordion-icon");
+  const content = item.querySelector(".citrusSpotlight__accordion-content");
+
+  title.addEventListener("click", () => {
+    const isActive = item.classList.contains("active");
+
+    // Close all accordion items except the clicked one
+    accordionItems.forEach((item) => {
+      if (item !== this) {
+        item.classList.remove("active");
+      }
+    });
+
+    if (!isActive) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+});
